8b251ad0ef37f9ffdabc4809cd372050aebfbf03
calmighted <calmighted@gmail.com>
2021-01-20 09:59:06 +0530

    added basic functionality

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
 let inputBtns = document.querySelectorAll('.Button');
 let inputDisplay = document.querySelector('.inputDisplay')
+let outputDisplay = document.querySelector('.outputDisplay')
 let Oper = document.querySelectorAll('.Button Oper')
-
+let expr = ""
+let Operation = ""
+let Num1 = ""
 
 function inputBtnHandler(e){
     if(e.target.className == "Button Clr" || e.target.className == "Button Del"){
             if(e.target.className == "Button Clr"){
                 inputDisplay.textContent = ""
+                outputDisplay.textContent = ""
             }else if (e.target.className == "Button Del"){
                 inputDisplay.textContent = inputDisplay.textContent.substr(0, inputDisplay.textContent.length - 1);
+                outputDisplay.textContent = ""
             }
         }
 
         else if (e.target.id == "equalTo"){
-            // console.log("Calculate")
-            console.log()
-            // operate() 
+            operate()
         }
         else{
+            if(e.target.className == "Button Oper"){
+                Operation = e.target.textContent;
+            }else if(e.target.className == "Button"){
+                Num1 += e.target.textContent
+                // console.log(Num1)
+            }
             inputDisplay.textContent += e.target.textContent
         }
     if(inputDisplay.textContent.length > 58){
         alert("Not more than 28 numbers pls")
         location.reload();
     }
+    expr = inputDisplay.textContent;
+    
 }
 
+let res;
 
-function operate(a,b,Operation){
-    console.log("Result")
+function operate(){
+    res = eval(expr)
+    console.log(res)
+    outputDisplay.textContent = res
 }
 
 
